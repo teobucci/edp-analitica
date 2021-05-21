277babc23cc2d58a232cb45e98ae5930f0cbefbf
Teo Bucci <teobucci8@gmail.com>
2021-05-21 21:00:36 +0200

    cap 4

--- matlab_code/edp_plot.m
+++ matlab_code/edp_plot.m
@@ -1,3 +1,5 @@
+set(0, 'DefaultFigureRenderer', 'painters');
+
 %% ----------------------------------------------------------------------------
 %% SEMAFORO
 ro_m = 1;
@@ -12,6 +14,8 @@ ro = ro_m.*(X <= -v_m * T)  + ro_m/2*(1 - X./(v_m*T)) .*( (-v_m*T<X) & (X < v_m*
 
 figure;
 mesh(X, T, ro, 'EdgeColor','black');
+xlabel('x')
+ylabel('t')
 
 %% ----------------------------------------------------------------------------
 %% TRAFFICO A VALLE
@@ -28,6 +32,8 @@ ro = 1/8*ro_m .* (X < -1/8 * v_m * T)  + ro_m .* (X > -1/8 * v_m * T);
 
 figure;
 mesh(X, T, ro, 'EdgeColor','black');
+xlabel('x')
+ylabel('t')
 
 %% ----------------------------------------------------------------------------
 %% CONTROESEMPIO BURGERS
@@ -38,10 +44,12 @@ interval_meno_03 = [-5 5 -0.3 -0.299 -5 5];
 interval_05 = [-5 5 0.499 0.5 -5 5];
 interval_15 = [-5 5 1.499 1.5 -5 5];
 figure()
-fimplicit3(U)
+fimplicit3(U,'EdgeColor','black','FaceAlpha',0)
 hold on
-title('Soluzione del controesempio di Burgers')
-fimplicit3(U, interval_meno_03,'g','LineWidth',5)
-fimplicit3(U, interval_05,'b', 'LineWidth',5)
-fimplicit3(U, interval_15,'r', 'LineWidth',5)
+%title('Soluzione del controesempio di Burgers')
+fimplicit3(U, interval_meno_03,'-','LineWidth',2)
+fimplicit3(U, interval_05,':', 'LineWidth',2)
+fimplicit3(U, interval_15,'-.', 'LineWidth',2)
 legend('surface', 't=-0,3', 't=0,5', 't=1')
+xlabel('x')
+ylabel('t')
